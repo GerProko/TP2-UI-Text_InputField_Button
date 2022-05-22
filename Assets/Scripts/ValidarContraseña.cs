78c4f809f7581ca348ac437421636f0d56875891
gerproko <101721169+GerProko@users.noreply.github.com>
2022-05-22 16:22:41 -0300

    Initial commit

---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+
+    string contraseñaCorrecta;
+    string contraseñaUsuario;
+    public Text ingresoUsuario;
+    public Text textocartelito;
+    public GameObject cartelitoMsj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseñaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContraseña()
+    {
+        contraseñaUsuario = ingresoUsuario.text;
+        if(contraseñaCorrecta==contraseñaUsuario)
+        {
+            cartelitoMsj.SetActive(true);
+            textocartelito.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textocartelito.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TP2-UI-Text_InputField_Button
+ TP2
